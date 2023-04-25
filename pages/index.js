55a6e7cc5e7bcf05d01a823a1f865a1e34fb43ad
tay-huang <thuang21@syr.edu>
2023-04-25 18:21:14 -0400

    import data from WordPress

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+/*import { useState } from 'react';
 
 import Button from '../components/Button';
 import CarColorPicker from '../components/CarColorPicker';
@@ -37,5 +37,44 @@ const Homepage = ({data,
     //<CarColorPicker colors={colors}/>
     //<SectionCTA />
   }</Layout>
+}
+export default Homepage
+*/
+
+import Layout from '../components/Layout';
+
+import { 
+  //getAllVehicles, 
+  //getAllColors 
+} from '../lib/api';
+
+export async function getStaticProps() {
+  // Get external data from the file system, API, DB, etc.
+  //const data = getAllVehicles();
+  //const colors = getAllColors();
+
+  // The value of the `props` key will be
+  //  passed to the `Home` component
+  return {
+    props: {
+      //data,
+      //colors
+    }
+  }
+}
+
+const Homepage = ({ 
+  //data, 
+  //colors
+}) => {
+  //console.log({data});
+  return <Layout>
+    {
+      //<Showcase />
+     //<LocationPicker />
+     //<CarColorPicker colors={colors} />
+     //<SectionCTA />
+    }
+  </Layout>
 }
 export default Homepage
